60d79fe00e3816c75b86620a9a0538233e5a4ce9
CJ Virtucio <cjv287@gmail.com>
2023-07-15 17:30:47 -0400

    added get_latest_policies.sh

--- bash_tools/bin/get_latest_policies.sh
+++ bash_tools/bin/get_latest_policies.sh
@@ -19,9 +19,19 @@ function main {
   local latest_attached_policy_versions=()
   local latest_attached_policy_version
   for attached_policy_arn in "${attached_policy_arns[@]}"; do
-    latest_attached_policy_version="$(aws iam list-policy-versions --policy-arn "${attached_policy_arn}" | jq -r '.Versions |= sort_by(.CreationDate) | .Versions[-1] | .VersionId')"
-    echo "latest_attached_policy_version: ${latest_attached_policy_version}"
+    latest_attached_policy_version="$(aws iam list-policy-versions --policy-arn "${attached_policy_arn}" | jq --raw-output --arg policy_arn "${attached_policy_arn}" '.Versions |= sort_by(.CreationDate) | .Versions[-1] | {PolicyArn: $policy_arn, VersionId: .VersionId}')"
+    latest_attached_policy_versions+=("${latest_attached_policy_version}")
   done
+
+  for latest_attached_policy_version in "${latest_attached_policy_versions[@]}"; do
+    local policy_arn
+    policy_arn="$(jq -r '.PolicyArn' <<<"${latest_attached_policy_version}")"
+
+    local version_id
+    version_id="$(jq -r '.VersionId' <<<"${latest_attached_policy_version}")"
+
+    aws iam get-policy-version --policy-arn "${policy_arn}" --version-id "${version_id}"
+  done | jq --slurp --arg resource_kind "${kind}" --arg resource_name "${name}" '{ResourceKind: $resource_kind, ResourceName: $resource_name, PolicyVersions: .}'
 }
 
 main "$@"
